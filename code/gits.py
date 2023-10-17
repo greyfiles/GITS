3d3115a8591ccb5689f5a71f36fc6a9546aff186
msreplog <msreplog@ncsu.edu>
2023-10-17 17:32:34 -0400

    wrap up work greyfiles/CSC510-Projects#1

--- code/gits.py
+++ code/gits.py
@@ -42,14 +42,9 @@ def cli():
     """gits: the wrapper CLI for git functions"""
     pass
 
-#parser = argparse.ArgumentParser()
-#subparsers = parser.add_subparsers()
 @cli.command('hello', short_help='should be hidden...', hidden=True)
 def hello():
     gits_hello_world()
-
-#gits_hello_subparser = subparsers.add_parser('hello_world')
-#gits_hello_subparser.set_defaults(func=gits_hello_world)
 
 @cli.command('set', short_help='set an environment variable')
 @click.argument('parent', nargs=1, type=click.STRING)
@@ -59,10 +54,6 @@ def set(parent):
     environment variables
     """
     gits_set_func(parent)
-
-#gits_set_subparser = subparsers.add_parser('set')
-#gits_set_subparser.add_argument('--parent', help='git parent branch')
-#gits_set_subparser.set_defaults(func=gits_set_func)
 
 @cli.command('add', short_help='add files to be committed')
 @click.argument('file_names', nargs=-1)
@@ -74,14 +65,6 @@ def add(file_names):
     add command
     """
     gits_add_func(file_names)
-
-#gits_add_subparser = subparsers.add_parser('add')
-#gits_add_subparser.add_argument('file_names',
-#                                metavar='N',
-#                                type=str,
-#                                nargs='+',
-#                                help='all file names')
-#gits_add_subparser.set_defaults(func=gits_add_func)
 
 @cli.command('commit', short_help='create a changeset with added files')
 @click.argument('message', nargs=1, type=click.STRING)
@@ -97,15 +80,6 @@ def commit(message, amend):
     """
     gits_commit_func(message, amend)
 
-#gits_commit_subparser = subparsers.add_parser('commit')
-#gits_commit_subparser.add_argument('-m',
-#                                   required=True,
-#                                   help='git commit message')
-#gits_commit_subparser.add_argument('--amend',
-#                                   action='store_true',
-#                                   help='amend commit message')
-#gits_commit_subparser.set_defaults(func=gits_commit_func)
-
 @cli.command('create', short_help='create a branch')
 @click.argument('name', nargs=1, type=click.STRING)
 def create(name):
@@ -117,10 +91,6 @@ def create(name):
     """
     create_branch(name)
 
-#gits_create_subparser = subparsers.add_parser('create')
-#gits_create_subparser.add_argument('-b', help="branch name to create")
-#gits_create_subparser.set_defaults(func=create_branch)
-
 @cli.command('switch', short_help='swap to a branch')
 @click.argument('branch_name', nargs=1, type=click.STRING)
 def switch(branch_name):
@@ -129,10 +99,6 @@ def switch(branch_name):
     """
     switch_branch(branch_name)
 
-#gits_switch_subparser = subparsers.add_parser('switch')
-#gits_switch_subparser.add_argument('branch_name', help="branch name to switch")
-#gits_switch_subparser.set_defaults(func=switch_branch)
-
 @cli.command('merge', short_help='merge a branch into current repo')
 @click.argument('branch_name', nargs=1, type=click.STRING)
 def merge(branch_name):
@@ -140,10 +106,6 @@ def merge(branch_name):
     Function that allows user to merge any branch into current branch
     """
     merge_branch(branch_name)
-
-#gits_merge_subparser = subparsers.add_parser('merge')
-#gits_merge_subparser.add_argument('branch_name', help="branch name to merge")
-#gits_merge_subparser.set_defaults(func=merge_branch)
 
 @cli.command('upstream', short_help='set an upstream branch')
 @click.option('--remote', nargs=1, type=click.STRING, help='remote branch to set as upstream')
@@ -154,15 +116,6 @@ def upstream(remote, local, upstream):
     Function that allows user to merge any branch into current branch
     """
     upstream(remote, local, upstream)
-
-#gits_upstream_subparser = subparsers.add_parser('upstream')
-#gits_upstream_subparser.add_argument('--remote',
-#                                     help='the remote branch name')
-#gits_upstream_subparser.add_argument('--local',
-#                                     help="local branch name")
-#gits_upstream_subparser.add_argument('--upstream',
-#                                     help="the upstream branch name")
-#gits_upstream_subparser.set_defaults(func=upstream)
 
 @cli.command('profile', short_help='setup git user')
 @click.argument('email', nargs=1, type=click.STRING)
@@ -176,15 +129,6 @@ def profile(email, name):
     """
     gits_set_profile(email, name)
 
-#gits_profile_subparser = subparsers.add_parser('profile', help='profie help')
-#gits_profile_subparser.set_defaults(func=gits_set_profile)
-#gits_profile_subparser.add_argument('--email',
-#                                    required=True,
-#                                    help='email to be used')
-#gits_profile_subparser.add_argument('--name',
-#                                    required=True,
-#                                    help='name to be used')
-
 @cli.command('super-reset', short_help='re-pull the repository')
 @click.option('--name', nargs=1, type=click.STRING)
 def super_reset(name):
@@ -193,20 +137,12 @@ def super_reset(name):
     """
     super_reset(name)
 
-#gits_super_reset_subparser = subparsers.add_parser('super-reset')
-#gits_super_reset_subparser.add_argument('--name',
-#                                        help="Name of the repository to super reset")
-#gits_super_reset_subparser.set_defaults(func=super_reset)
-
 @cli.command('rebase', short_help='rebase from master')
 def rebase():
     """
     Function that allows user to interactively rebase from the master branch
     """
     gits_rebase()
-
-#gits_rb_subparser = subparsers.add_parser('rebase', help='sync help')
-#gits_rb_subparser.set_defaults(func=gits_rebase)
 
 @cli.command('status', short_help='print current working status')
 def status():
@@ -215,9 +151,6 @@ def status():
     """
     gits_status()
 
-#gits_status_subparser = subparsers.add_parser('status', help='sync help')
-#gits_status_subparser.set_defaults(func=gits_status)
-
 @cli.command('diff', short_help='show differences from last commit')
 def diff():
     """
@@ -225,18 +158,12 @@ def diff():
     """
     gits_diff()
 
-#gits_diff_subparser = subparsers.add_parser('diff', help='sync help')
-#gits_diff_subparser.set_defaults(func=gits_diff)
-
 @cli.command('branch', short_help='show all local branches')
 def branch():
     """
     Function that outputs the branches currently pulled in the local repo
     """
     gits_branch()
-
-#gits_branch_subparser = subparsers.add_parser('branch', help='sync help')
-#gits_branch_subparser.set_defaults(func=gits_branch)
 
 @cli.command('reset', short_help='hard reset a branch')
 @click.argument('branch', nargs=1, type=click.STRING)
@@ -247,11 +174,6 @@ def merge(branch):
     BRANCH is the name of the branch to be reset
     """
     gits_reset(branch)
-
-#gits_reset_subparser = subparsers.add_parser('reset', help='sync help')
-#gits_reset_subparser.set_defaults(func=gits_reset)
-#gits_reset_subparser.add_argument(
-#    '--branch', required=True, help='branch to be used')
 
 @cli.command('delete', short_help='delete a commit from remote')
 @click.argument('branch', nargs=1, type=click.STRING)
@@ -265,14 +187,6 @@ def delete(branch, count):
     """
     gits_delete(branch, count)
 
-#gits_reset_subparser = subparsers.add_parser('delete', help='sync help')
-#gits_reset_subparser.set_defaults(func=gits_delete)
-#gits_reset_subparser.add_argument(
-#    '--branch', required=True, help='branch to be used')
-#gits_reset_subparser.add_argument(
-#    '--count', required=True, help='Last commits to be deleted')
-#
-
 @cli.command('track', short_help='add untracked files to working changes')
 @click.argument('file_names', nargs=-1)
 def track(file_names):
@@ -280,14 +194,6 @@ def track(file_names):
     Function that adds the passed files to the working set
     """
     gits_track(file_names)
-
-#gits_track_subparser = subparsers.add_parser('track')
-#gits_track_subparser.add_argument('file_names',
-#                                  metavar='N',
-#                                  type=str,
-#                                  nargs='+',
-#                                  help='all file names')
-#gits_track_subparser.set_defaults(func=gits_track)
 
 @cli.command('untrack', short_help='remove files from working changes')
 @click.argument('file_names', nargs=-1)
@@ -297,14 +203,6 @@ def untrack(file_names):
     """
     gits_untrack(file_names)
 
-#gits_untrack_subparser = subparsers.add_parser('untrack')
-#gits_untrack_subparser.add_argument('file_names',
-#                                    metavar='N',
-#                                    type=str,
-#                                    nargs='+',
-#                                    help='all file names')
-#gits_untrack_subparser.set_defaults(func=gits_untrack)
-
 @cli.command('undo', short_help='un-stage files added to staging directory')
 @click.argument('file_names', nargs=-1)
 def undo(file_names):
@@ -312,14 +210,6 @@ def undo(file_names):
     Function that moves files from the staging directory to the working directory
     """
     gits_undo(file_names)
-
-#gits_undo_subparser = subparsers.add_parser('undo')
-#gits_undo_subparser.add_argument('file_names',
-#                                 metavar='N',
-#                                 type=str,
-#                                 nargs='+',
-#                                 help='all file names')
-#gits_undo_subparser.set_defaults(func=gits_undo)
 
 @cli.command('sync', short_help='get current state of trunk')
 @click.option('--source', nargs=1, type=click.STRING, help='name of the trunk branch')
@@ -329,10 +219,6 @@ def sync(source):
     """
     gits_sync(source)
 
-#gits_sync_subparser = subparsers.add_parser('sync')
-#gits_sync_subparser.add_argument('-source', help="name of the trunk branch")
-#gits_sync_subparser.set_defaults(func=gits_sync)
-
 @cli.command('push', short_help='get current state of trunk')
 @click.option("--rebase", nargs=1, default=False, help="do a pull rebase before pushing the changes")
 def push(rebase):
@@ -340,12 +226,6 @@ def push(rebase):
     Function that sends the record of local commits to remote
     """
     gits_push(rebase)
-
-#gits_push_subparser = subparsers.add_parser('push')
-#gits_push_subparser.add_argument("--rebase", nargs=1, default=False,
-#                                 help="do a pull rebase before pushing the changes",
-#                                 required=False)
-#gits_push_subparser.set_defaults(func=gits_push)
 
 @cli.command('init', short_help='setup current folder as git repository')
 @click.option("--bare", is_flag=True, default=False, help='intialize an empty git repositories but omit the working directory')
@@ -357,15 +237,6 @@ def init(bare, template, clone_url):
     """
     gits_init(bare, template, clone_url)
 
-#gits_init_subparser = subparsers.add_parser("init")
-#gits_init_subparser.add_argument("--bare", action="store_true",
-#                                 help="intialize an empty git repositories but omit the working directory")
-#gits_init_subparser.add_argument(
-#    "--template", help="initialize a git repository using predifined templates")
-#gits_init_subparser.add_argument(
-#    "--clone_url", help="url for cloning an already existing repo")
-#gits_init_subparser.set_defaults(func=gits_init)
-
 @cli.command('pull', short_help='get latest state from remote')
 @click.option("--nocommit", is_flag=True, default=False, help='fetch remote without making a merge commit')
 @click.option("--rebase", is_flag=True, default=False, help='use git rebase to merge with remote branch')
@@ -376,18 +247,6 @@ def pull(nocommit, template, branch):
     """
     gits_pull(nocommit, template, branch)
 
-#gits_pull_subparser = subparsers.add_parser("pull")
-#gits_pull_subparser.add_argument("--nocommit", action='store_true',
-#                                 help="fetches the remote contain but does not create a new merge commit",
-#                                 required=False)
-#gits_pull_subparser.add_argument("--rebase", action='store_true',
-#                                 help="uses git rebase to merge with the remote branch",
-#                                 required=False)
-#gits_pull_subparser.add_argument("--branch", nargs="?", default=False,
-#                                 help="you can specify the branch you want to pull",
-#                                 required=False)
-#gits_pull_subparser.set_defaults(func=gits_pull)
-
 @cli.command('super-init', short_help='setup and initialize a repo')
 @click.option("--readme-name", nargs=1, default=False, help='custom name for README')
 @click.option('--gitignore-content', nargs=1, default=False, help='initial contents of gitignore file')
@@ -397,23 +256,6 @@ def super_init(readme_name, gitignore_content, remote_url):
     Function that initializes and adds files to a generic repository
     """
     gits_super_init(readme_name, gitignore_content, remote_url)
-
-#gits_super_init_subparser = subparsers.add_parser('super_init', 
-#    help='Initializes a new git repo, adds default README.md and .gitignore, and commits them.')
-#gits_super_init_subparser.add_argument('--readme-name', default='README.md',
-#    help='Custom name for the README file. Default is README.md.')
-#gits_super_init_subparser.add_argument('--gitignore-content', default='',
-#    help='Content to add in the .gitignore file. By default, it will be empty.')
-#gits_super_init_subparser.set_defaults(func=gits_super_init)
-#
-#
-#args = parser.parse_args()
-
-#if hasattr(args, 'func'):
-#    args.func(args)
-#else:
-#    parser.print_help()
-#    sys.exit(1)
 
 cli(prog_name='gits')
 
